a5cb5197e218605c8662edbd65afe45804aad725
Agustin Burset <agustin.burset@icloud.com>
2020-04-23 15:50:46 -0300

    Update shaders to use positions as color (looks nice)

--- utils/shaders.js
+++ utils/shaders.js
@@ -3,8 +3,10 @@ export const vertexShaderSourceCode = `#version 300 es
   uniform mat4 modelMatrix;
 
   in vec2 vertexPosition;
+  out vec3 color;
 
   void main() {
+    color = vec3(vertexPosition, 1);
     gl_Position = modelMatrix * vec4(vertexPosition, 0, 1);
   }
 `
@@ -12,9 +14,10 @@ export const vertexShaderSourceCode = `#version 300 es
 export const fragmentShaderSourceCode = `#version 300 es
   precision mediump float;
 
+  in vec3 color;
   out vec4 fragmentColor;
 
   void main() {
-    fragmentColor = vec4(0, 1, 0, 1);
+    fragmentColor = vec4(color, 1);
   }
 `
